a33daf10d949cfdd4069121a60de5ada740be5a4
Simon Cropp <simon.cropp@gmail.com>
2017-08-06 19:43:54 +1000

    add byte encryption

--- Newtonsoft.Json.Encryption/StringEncrypt.cs
+++ Newtonsoft.Json.Encryption/StringEncrypt.cs
@@ -59,6 +59,44 @@ namespace Newtonsoft.Json.Encryption
                 cryptoCleanup(decryptor);
             }
         }
+        public byte[] Encrypt(byte[] target)
+        {
+            var encryptor = encryptProvider();
+            try
+            {
+
+                return PerformCryptography(encryptor, target);
+            }
+            finally
+            {
+                cryptoCleanup(encryptor);
+            }
+        }
+
+        public byte[] Decrypt(byte[] value)
+        {
+            var decryptor = decryptProvider();
+            try
+            {
+                return PerformCryptography(decryptor, value);
+            }
+            finally
+            {
+                cryptoCleanup(decryptor);
+            }
+        }
+        byte[] PerformCryptography(ICryptoTransform cryptoTransform, byte[] data)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(data, 0, data.Length);
+                    cryptoStream.FlushFinalBlock();
+                    return memoryStream.ToArray();
+                }
+            }
+        }
 
     }
 }
