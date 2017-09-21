a06b527b1be186fa036c835b53391411dcb077d2
Simon Cropp <simon.cropp@gmail.com>
2017-09-21 16:16:47 +1000

    add example usage

--- Tests/Sessions/ThreadLocalSessionTest.cs
+++ Tests/Sessions/ThreadLocalSessionTest.cs
@@ -1,4 +1,6 @@
-﻿using ApprovalTests;
+﻿using System.Security.Cryptography;
+using System.Text;
+using ApprovalTests;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Encryption;
 using NUnit.Framework;
@@ -7,6 +9,60 @@ using ObjectApproval;
 [TestFixture]
 public class ThreadLocalSessionTest
 {
+    [Test]
+    public void ExampleUsage()
+    {
+        // per system (rotated)
+        var key = Encoding.UTF8.GetBytes("gdDbqRpqdRbTs3mhdZh9qCaDaxJXl+e6");
+
+        // per app domain
+        var serializer = new JsonSerializer
+        {
+            ContractResolver = new ContractResolver(
+                encrypter: new Encrypter(
+                    encryptProvider: ThreadLocalSession.GetEncryptProvider(),
+                    decryptProvider: ThreadLocalSession.GetDecryptProvider(),
+                    encryptCleanup: ThreadLocalSession.GetEncryptCleanup(),
+                    decryptCleanup: ThreadLocalSession.GetDecryptCleanup())
+            )
+        };
+
+        // transferred as meta data with the serialized payload
+        byte[] iv;
+
+        string serialized;
+        // per serialize session
+        using (var algorithm = new RijndaelManaged
+        {
+            Key = key
+        })
+        {
+            iv = algorithm.IV;
+            using (new ThreadLocalSession(algorithm))
+            {
+                var instance = new ClassToSerialize
+                {
+                    Property = "A"
+                };
+                serialized = serializer.Serialize(instance);
+            }
+        }
+
+        // per deserialize session
+        using (var algorithm = new RijndaelManaged
+        {
+            IV = iv,
+            Key = key
+        })
+        {
+            using (new ThreadLocalSession(algorithm))
+            {
+                var deserialized = serializer.Deserialize<ClassToSerialize>(serialized);
+                ObjectApprover.VerifyWithJson(deserialized);
+            }
+        }
+    }
+
     [Test]
     public void Simple()
     {
