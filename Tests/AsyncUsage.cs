f42ee7ed4f10bbc49126179706cc24dcc5b1379e
danielmarbach <daniel.marbach@openplace.net>
2017-09-22 12:01:41 +0200

    New async roundtrip method and test

---
+++ Tests/AsyncUsage.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+[TestFixture]
+public class AsyncUsage
+{
+    [Test]
+    public async Task Works()
+    {
+        var target = new SimpleUsage.ClassWithString
+        {
+            Property = "Foo"
+        };
+        var result = await RoundTrip.RunAsync(target);
+        Assert.AreEqual("Foo", result.Property);
+    }
+}
--- Tests/RoundTrip.cs
+++ Tests/RoundTrip.cs
@@ -1,4 +1,5 @@
-﻿using ApprovalTests;
+﻿using System.Threading.Tasks;
+using ApprovalTests;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Encryption;
 
@@ -6,23 +7,51 @@ public static class RoundTrip
 {
     public static T Run<T>(T instance)
     {
+        using(var factory = new EncryptionFactory())
         using (var algorithm = CryptoBuilder.Build())
         {
             var serializer = new JsonSerializer
             {
-                ContractResolver = new ContractResolver(
-                    encrypter: new Encrypter(
-                        encryptProvider: () => algorithm.CreateEncryptor(),
-                        decryptProvider: () => algorithm.CreateDecryptor(),
-                        encryptCleanup: transform => { transform.Dispose(); },
-                        decryptCleanup: transform => { transform.Dispose(); })
-                )
+                ContractResolver = factory.GetContractResolver()
             };
 
-            var result = serializer.Serialize(instance);
+            string result;
+            using (factory.GetEncryptSession(algorithm))
+            {
+                result = serializer.Serialize(instance);
+            }
 
             Approvals.Verify(result);
-            return serializer.Deserialize<T>(result);
+            using (factory.GetDecryptSession(algorithm))
+            {
+                return serializer.Deserialize<T>(result);
+            }
+        }
+    }
+
+    public static async Task<T> RunAsync<T>(T instance)
+    {
+        using (var factory = new EncryptionFactory())
+        using (var algorithm = CryptoBuilder.Build())
+        {
+            var serializer = new JsonSerializer
+            {
+                ContractResolver = factory.GetContractResolver()
+            };
+
+            string result;
+            using (factory.GetEncryptSession(algorithm))
+            {
+                await Task.Delay(1).ConfigureAwait(false);
+                result = serializer.Serialize(instance);
+            }
+
+            Approvals.Verify(result);
+            using (factory.GetDecryptSession(algorithm))
+            {
+                await Task.Delay(1).ConfigureAwait(false);
+                return serializer.Deserialize<T>(result);
+            }
         }
     }
 }
